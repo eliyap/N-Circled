7aaefdcb7ecfaa522d90aa8e3b9259f884feedbb
eliyap <yap@usc.edu>
2022-04-06 14:12:41 -0700

    use multiple coordinated spinnners

--- N-Circled.swiftpm/SpinnerView.swift
+++ N-Circled.swiftpm/SpinnerView.swift
@@ -11,11 +11,12 @@ struct SpinnerView: View {
     
     public let dftResults: [Complex<Float>] = test()
     
-    private let spinner: Spinner? = .init(
-        amplitude: 1,
-        frequency: 1,
-        phase: .zero
-    )
+    private let spinners: [Spinner] = [
+        .init(amplitude: 0.25, frequency: 1, phase: .zero),
+        .init(amplitude: 0.50, frequency: 2, phase: .zero),
+        .init(amplitude: 0.75, frequency: 3, phase: .zero),
+        .init(amplitude: 1.00, frequency: 4, phase: .zero),
+    ]
     
     var body: some View {
         TimelineView(.animation) { timeline in
@@ -23,31 +24,24 @@ struct SpinnerView: View {
                 let location = dftResults.fourierSeriesResult(date: timeline.date, size: size)
                 context.draw(Image(systemName: "heart"), at: location)
                 
-                if let spinner = spinner {
-                    let center = CGPoint(x: size.width / 2, y: size.height / 2)
-
-
-                    var loc = spinner.radians(at: timeline.date).remainder(dividingBy: 1)
-                    loc = loc < 0 ? loc + 1 : loc
-                    let shading: GraphicsContext.Shading = .conicGradient(
-                        spinner.gradient(at: timeline.date),
-                        center: center,
-                        angle: Angle(radians: 1) * spinner.radians(at: timeline.date)
-                    )
-
-                    let diameter = spinner.amplitude * min(size.width, size.height)
-                    let square = CGRect(
-                        origin: CGPoint(
-                            x: center.x - diameter / 2,
-                            y: center.y - diameter / 2
-                        ),
-                        size: CGSize(width: diameter, height: diameter)
-                    )
-                    let path = Circle().path(in: square)
-
-                    context.stroke(path, with: shading)
+                var offset: CGPoint = .zero
+                for spinner in spinners {
+                    spinner.draw(in: &context, date: timeline.date, size: size, offset: offset)
+//                    offset.x += spinner.offset(at: timeline.date).x * min(size.height, size.height)
+//                    offset.y += spinner.offset(at: timeline.date).y * min(size.height, size.height)
                 }
             }
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(spinners) { spinner in
+                        Canvas { (context, size) in
+                            spinner.draw(in: &context, date: timeline.date, size: size, offset: .zero)
+                        }
+                            .aspectRatio(1, contentMode: .fit)
+                    }
+                }
+            }
+                .frame(height: 100)
         }
     }
 }
@@ -79,6 +73,9 @@ extension Collection where Element == Complex<Float> {
 
 /// Represents a uniformly rotating circle with some size, at some phase.
 struct Spinner {
+    
+    let id: UUID = .init()
+    
     /// Non-negative by convention.
     /// Normalized to `[0, 1]`.
     let amplitude: CGFloat
@@ -107,9 +104,13 @@ struct Spinner {
     }
 }
 
+extension Spinner: Identifiable {
+    
+}
+
 extension Spinner {
     func gradient(at date: Date) -> SwiftUI.Gradient {
-        var proportion = radians(at: date)
+        var proportion = radians(at: date) + .pi / 2
         proportion /= .pi * 2
         proportion = proportion.remainder(dividingBy: 1)
         proportion = proportion < 0
@@ -119,11 +120,44 @@ extension Spinner {
         
         let zeroColor = SwiftUI.Color(UIColor.red.withAlphaComponent(proportion))
         
+        /// A tiny increment designed to create a sharp break between clear and opaque.
+        /// If incrementing by this would exceed 1, don't do that.
+        /// This fixes a runtime error `stops must be ordered.`
+        let marginal = 0.001
+        if proportion + marginal > 1 {
+            proportion -= marginal
+        }
+        
         return SwiftUI.Gradient(stops: [
             .init(color: zeroColor, location: 0),
             .init(color: SwiftUI.Color(UIColor.clear), location: proportion),
             .init(color: SwiftUI.Color(UIColor.red), location: proportion + 0.001),
             .init(color: zeroColor, location: 1),
         ])
+    }
+    
+    func draw(in context: inout GraphicsContext, date: Date, size: CGSize, offset: CGPoint) -> Void {
+        let center = CGPoint(
+            x: offset.x + size.width / 2,
+            y: offset.y + size.height / 2
+        )
+
+        let shading: GraphicsContext.Shading = .conicGradient(
+            gradient(at: date),
+            center: center,
+            angle: Angle(radians: 1) * radians(at: date)
+        )
+
+        let diameter = amplitude * min(size.width, size.height)
+        let square = CGRect(
+            origin: CGPoint(
+                x: center.x - diameter / 2,
+                y: center.y - diameter / 2
+            ),
+            size: CGSize(width: diameter, height: diameter)
+        )
+        let path = Circle().path(in: square)
+
+        context.stroke(path, with: shading)
     }
 }
