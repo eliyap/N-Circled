e7edcf8171bd503b0213f6e2b47a159e64bded1c
eliyap <yap@usc.edu>
2022-04-06 18:30:06 -0700

    add a small safety wrapper

--- N-Circled.swiftpm/CASpinner.swift
+++ N-Circled.swiftpm/CASpinner.swift
@@ -39,12 +39,24 @@ final class CASpinnerView: UIView {
         animation.toValue = CATransform3DRotate(transform, CGFloat(90 * Double.pi / 180.0), 0, 1, 0)
         animation.duration = 1.25
 
-        shapeLayer.add(animation, forKey: "transform")
+        shapeLayer.add(animation, property: .transform)
         
         layer.addSublayer(shapeLayer)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension CALayer {
+    /// A safer view into `CALayer`'s string-ly typed keys.
+    /// A complete list: https://stackoverflow.com/questions/44230796/what-is-the-full-keypath-list-for-cabasicanimation
+    enum AnimatableProperty: String {
+        case transform
+    }
+    
+    func add(_ animation: CAAnimation, property: AnimatableProperty) -> Void {
+        self.add(animation, forKey: property.rawValue)
     }
 }
