29f53f0ddfd0a33ca597f03d5782fbc40cda9d99
eliyap <yap@usc.edu>
2022-04-07 13:39:20 -0700

    draft state passing and stroke analysis

--- N-Circled.swiftpm/ContentView.swift
+++ N-Circled.swiftpm/ContentView.swift
@@ -1,13 +1,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject private var spinnerHolder: SpinnerHolder = .init()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, WWDC!")
-            DoodleView()
+            Text("\(spinnerHolder.spinners.count)")
+            DoodleView(spinnerHolder: spinnerHolder)
                 .border(Color.red)
                 .onAppear(perform: {
                     test()
@@ -18,4 +22,8 @@ struct ContentView: View {
                 .border(Color.red)
         }
     }
+}
+
+final class SpinnerHolder: ObservableObject {
+    @Published var spinners: [Spinner] = []
 }
--- N-Circled.swiftpm/DoodleView.swift
+++ N-Circled.swiftpm/DoodleView.swift
@@ -12,8 +12,10 @@ import PencilKit
 struct DoodleView: UIViewControllerRepresentable {
     typealias UIViewControllerType = UIDoodleViewController
     
+    public var spinnerHolder: SpinnerHolder
+    
     func makeUIViewController(context: Context) -> UIDoodleViewController {
-        let vc: UIViewControllerType = .init()
+        let vc: UIViewControllerType = .init(spinnerHolder: spinnerHolder)
         return vc
     }
     
@@ -25,16 +27,41 @@ struct DoodleView: UIViewControllerRepresentable {
 final class UIDoodleViewController: UIViewController {
     
     private let doodleView: UIDoodleView
+    private let spinnerHolder: SpinnerHolder
     
-    init() {
+    init(spinnerHolder: SpinnerHolder) {
         self.doodleView = .init()
+        self.spinnerHolder = spinnerHolder
         super.init(nibName: nil, bundle: nil)
         
         self.view = doodleView
+        doodleView.delegate = self
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension UIDoodleViewController: PKCanvasViewDelegate {
+    func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
+        print(strokePoints(canvasView: canvasView).count)
+    }
+    
+    private func strokePoints(canvasView: PKCanvasView) -> [CGPoint] {
+        let epsilon = 0.001
+        
+        var points: [CGPoint] = []
+        
+        for stroke in canvasView.drawing.strokes {
+            guard stroke.path.isEmpty == false else { continue }
+            for proportion in stride(from: 0.0, to: 1.0, by: epsilon) {
+                let location = stroke.path.interpolatedLocation(at: CGFloat(stroke.path.endIndex) * proportion)
+                points.append(location)
+            }
+        }
+        
+        return points
     }
 }
 
