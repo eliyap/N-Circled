7391abff6f5f78eba8f518c2883df903c4577c59
eliyap <yap@usc.edu>
2022-04-09 16:15:49 -0700

    clone spinner func

--- N-Circled.swiftpm/SpinnerHolder.swift
+++ N-Circled.swiftpm/SpinnerHolder.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import ComplexModule
 
 final class SpinnerHolder: ObservableObject {
     
@@ -15,5 +16,36 @@ final class SpinnerHolder: ObservableObject {
     
     init() {
         
+    }
+}
+
+func makeSpinners(from points: [CGPoint]) -> [Spinner]? {
+    let values = points.map { point in
+        return Complex<Float>(Float(point.x), Float(point.y))
+    }
+    let dft = testAccelerate(values: values)
+    var spinners: [Spinner] = []
+    for (idx, complex) in dft.enumerated() {
+        guard complex.phase.isNaN == false else { continue }
+        
+        var amp = CGFloat(complex.length) / CGFloat(values.count)
+        amp /= 100
+        #warning("temp damper")
+        
+        let spinner = Spinner(
+            amplitude: amp,
+            frequency: idx,
+            phase: CGFloat(complex.phase)
+        )
+        #warning("todo fix phase")
+        
+        spinners.append(spinner)
+    }
+    
+    if spinners.isEmpty == false {
+        /// Remove the first, zero frequency component.
+        return Array(spinners[1...])
+    } else {
+        return nil
     }
 }
