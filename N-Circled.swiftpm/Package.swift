f38e95f99fbf2a8d51dd1360a0a524a9b61741c5
eliyap <yap@usc.edu>
2022-04-10 13:01:20 -0700

    disable non portrait orientations
    breaks our CA layers

--- N-Circled.swiftpm/Package.swift
+++ N-Circled.swiftpm/Package.swift
@@ -28,9 +28,6 @@ let package = Package(
             ],
             supportedInterfaceOrientations: [
                 .portrait,
-                .landscapeRight,
-                .landscapeLeft,
-                .portraitUpsideDown(.when(deviceFamilies: [.pad]))
             ]
         )
     ],
