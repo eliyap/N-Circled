9fb295d2470ee2bc7578bac3d3e062f611198795
eliyap <yap@usc.edu>
2022-04-10 15:24:46 -0700

    break up file

--- N-Circled.swiftpm/Twiddler/SpinnerEditView.swift
+++ N-Circled.swiftpm/Twiddler/SpinnerEditView.swift
@@ -1,47 +1,11 @@
 //
-//  File.swift
+//  SpinnerEditView.swift
 //  N-Circled
 //
 //  Created by Secret Asian Man Dev on 10/4/22.
 //
 
 import SwiftUI
-import ComplexModule
-
-struct TwiddlerCollectionView: View {
-    
-    @ObservedObject public var spinnerHolder: SpinnerHolder
-    
-    var body: some View {
-        ScrollView(.horizontal) {
-            HStack(spacing: .zero) {
-                ForEach($spinnerHolder.spinners) { $spinner in
-                    SpinnerThumbnailView(spinner: $spinner)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .aspectRatio(1, contentMode: .fit)
-                        .border(.red)
-                }
-            }
-        }
-    }
-}
-
-struct SpinnerThumbnailView: View {
-    
-    @Binding public var spinner: Spinner
-    
-    @State private var isEditing: Bool = false
-    
-    var body: some View {
-        Text("s")
-            .onTapGesture(perform: {
-                isEditing = true
-            })
-            .fullScreenCover(isPresented: $isEditing, content: {
-                SpinnerEditView.init(spinner: $spinner)
-            })
-    }
-}
 
 struct SpinnerEditView: View {
     
