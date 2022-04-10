3f15e79a37905bc6aa66e899e1e5ce91aa44bad2
eliyap <yap@usc.edu>
2022-04-10 12:46:19 -0700

    new view

---
+++ N-Circled.swiftpm/Twiddler.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  N-Circled
+//
+//  Created by Secret Asian Man Dev on 10/4/22.
+//
+
+import SwiftUI
+
+struct TwiddlerCollectionView: View {
+    
+    
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack(spacing: .zero) {
+                
+            }
+        }
+    }
+}
