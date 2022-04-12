b3751b039dda76479e4f98180709b16e14213b31
eliyap <yap@usc.edu>
2022-04-11 17:07:04 -0700

    adjust spinners

--- N-Circled.swiftpm/SpinnerHolder.swift
+++ N-Circled.swiftpm/SpinnerHolder.swift
@@ -10,8 +10,8 @@ import UIKit.UIColor
 
 final class SpinnerHolder: ObservableObject {
     @Published var spinners: [Spinner] = [
-        Spinner(amplitude: 0.5, frequency: +1, phase: .pi / 10, color: UIColor.green.cgColor),
-        Spinner(amplitude: 0.4, frequency: +2, phase: .pi / 17, color: UIColor.yellow.cgColor),
+        Spinner(amplitude: 0.2, frequency: -1, phase: .pi / 10, color: UIColor.green.cgColor),
+        Spinner(amplitude: 0.6, frequency: +1, phase: .pi / 17, color: UIColor.yellow.cgColor),
         Spinner(amplitude: 0.3, frequency: +3, phase: .pi / 20, color: UIColor.orange.cgColor),
         Spinner(amplitude: 0.2, frequency: +4, phase: .pi / 6, color: UIColor.purple.cgColor),
         Spinner(amplitude: 0.1, frequency: +5, phase: .pi / 4, color: UIColor.blue.cgColor),
