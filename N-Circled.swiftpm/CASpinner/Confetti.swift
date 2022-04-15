46396c86db14cc98090d1f3ba77f453a4a95c1d9
eliyap <yap@usc.edu>
2022-04-14 17:08:41 -0700

    picky refactor

--- N-Circled.swiftpm/CASpinner/Confetti.swift
+++ N-Circled.swiftpm/CASpinner/Confetti.swift
@@ -40,7 +40,13 @@ public class SwiftConfettiView: UIView {
     }
 
     var emitter: CAEmitterLayer!
-    public var colors: [UIColor]!
+    public var colors: [UIColor] = [
+        UIColor(red:0.95, green:0.40, blue:0.27, alpha:1.0),
+        UIColor(red:1.00, green:0.78, blue:0.36, alpha:1.0),
+        UIColor(red:0.48, green:0.78, blue:0.64, alpha:1.0),
+        UIColor(red:0.30, green:0.76, blue:0.85, alpha:1.0),
+        UIColor(red:0.58, green:0.39, blue:0.55, alpha:1.0)
+    ]
     public var intensity: Float!
     public var type: ConfettiType!
     private var active :Bool!
@@ -51,11 +57,6 @@ public class SwiftConfettiView: UIView {
 
     public override init(frame: CGRect) {
         super.init(frame: frame)
-        colors = [UIColor(red:0.95, green:0.40, blue:0.27, alpha:1.0),
-            UIColor(red:1.00, green:0.78, blue:0.36, alpha:1.0),
-            UIColor(red:0.48, green:0.78, blue:0.64, alpha:1.0),
-            UIColor(red:0.30, green:0.76, blue:0.85, alpha:1.0),
-            UIColor(red:0.58, green:0.39, blue:0.55, alpha:1.0)]
         intensity = 0.5
         type = .confetti
         active = false
@@ -133,6 +134,6 @@ public class SwiftConfettiView: UIView {
     }
 
     public func isActive() -> Bool {
-            return self.active
+        return self.active
     }
 }
