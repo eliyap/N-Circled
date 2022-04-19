1bfcb0b8bd173127a26d30dbfaa0808f9d2439cf
eliyap <yap@usc.edu>
2022-04-19 07:18:08 -0700

    Create PuzzleManager.swift

---
+++ N-Circled.swiftpm/Storage/PuzzleManager.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Secret Asian Man Dev on 19/4/22.
+//
+
+import Foundation
+import Combine
+
+internal final class PuzzleManager: ObservableObject {
+    @Published public var puzzles: [Puzzle] {
+        didSet {
+            /// Save to disk whenever modified.
+            UserDefaults.groupSuite.puzzles = puzzles
+        }
+    }
+    
+    init() {
+        /// Load on app start.
+        self.puzzles = UserDefaults.groupSuite.puzzles
+    }
+}
