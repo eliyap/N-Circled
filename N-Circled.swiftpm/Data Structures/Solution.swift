da60a6f125a19787a5dfa40fd5f79c9596636af7
eliyap <yap@usc.edu>
2022-04-18 18:09:48 -0700

    split file

--- N-Circled.swiftpm/Data Structures/Solution.swift
+++ N-Circled.swiftpm/Data Structures/Solution.swift
@@ -8,44 +8,6 @@
 import CoreGraphics
 import KDTree
 
-struct Scorer {
-    let base: Double
-    let maxExponent: Double
-    let scalar: Double
-    
-    init(
-        base: Double = 2.0,
-        maxExponent: Double = 2.0,
-        scalar: Double = 10.0
-    ) {
-        self.base = base
-        self.maxExponent = maxExponent
-        self.scalar = scalar
-    }
-    
-    /** Scores a distance.
-     *  Scoring goals:
-     *  - higher is better, better is higher
-     *  - should always be positive
-     *  - bounded values (has a minimum and maximum)
-     * 
-     *  Exponents satisfy all these criteria.
-     */
-    func score(distance: Double) -> Double { 
-        assert(distance >= 0, "Received negative distance: \(distance)")
-        
-        return pow(base, maxExponent - (scalar * distance))
-    }
-
-    func normalizedScore(distance: Double) -> Double {
-        return score(distance: distance) / maxScore
-    }
-
-    var maxScore: Double {
-        return pow(base, maxExponent)
-    }
-}
-
 struct Solution {
     let spinners: [Spinner]
     
