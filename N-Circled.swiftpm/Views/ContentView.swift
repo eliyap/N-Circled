c245d44aa4685bef13b795d237080e569931c35f
eliyap <yap@usc.edu>
2022-04-21 14:31:42 -0700

    add reset alerts

--- N-Circled.swiftpm/Views/ContentView.swift
+++ N-Circled.swiftpm/Views/ContentView.swift
@@ -4,6 +4,12 @@ struct ContentView: View {
     
     @StateObject private var puzzleManager: PuzzleManager = .init()
     
+    
+    #if DEBUG
+    @State private var didResetPuzzles: Bool = false
+    @State private var didUnlockPuzzles: Bool = false
+    #endif
+    
     var body: some View {
         NavigationView {
             List {
@@ -21,20 +27,29 @@ struct ContentView: View {
                         /// Reset puzzle state.
                         Button(action: {
                             puzzleManager.puzzles = Puzzle.initialSet
+                            didResetPuzzles = true
                         }, label: {
                             Image(systemName: "circle.slash")
                                 .font(.body.bold())
                         })
+                            .alert("DEBUG: Reset All Puzzles", isPresented: $didResetPuzzles) {
+                                Button("OK", role: .cancel) { }
+                            }
+
                         
                         /// Unlock all puzzles.
                         Button(action: {
                             for idx in 0..<puzzleManager.puzzles.count {
                                 puzzleManager.puzzles[idx].unlocked = true
+                                didUnlockPuzzles = true
                             }
                         }, label: {
                             Image(systemName: "lock.open")
                                 .font(.body.bold())
                         })
+                            .alert("DEBUG: Unlocked All Puzzles", isPresented: $didUnlockPuzzles) {
+                                Button("OK", role: .cancel) { }
+                            }
                     })
                 })
                 #endif
