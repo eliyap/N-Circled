76f35492d65598981fdeb6ad66ce36f8b5ddc5d1
eliyap <yap@usc.edu>
2022-04-21 14:28:13 -0700

    add an unlock button

--- N-Circled.swiftpm/Views/ContentView.swift
+++ N-Circled.swiftpm/Views/ContentView.swift
@@ -17,13 +17,26 @@ struct ContentView: View {
             }
                 #if DEBUG
                 .toolbar(content: {
-                    /// Reset puzzle state.
-                    Button(action: {
-                        puzzleManager.puzzles = Puzzle.initialSet
-                    }, label: {
-                        Image(systemName: "circle.slash")
-                            .font(.body.bold())
+                    ToolbarItemGroup(placement: .navigationBarTrailing, content: {
+                        /// Reset puzzle state.
+                        Button(action: {
+                            puzzleManager.puzzles = Puzzle.initialSet
+                        }, label: {
+                            Image(systemName: "circle.slash")
+                                .font(.body.bold())
+                        })
+                        
+                        /// Unlock all puzzles.
+                        Button(action: {
+                            for idx in 0..<puzzleManager.puzzles.count {
+                                puzzleManager.puzzles[idx].unlocked = true
+                            }
+                        }, label: {
+                            Image(systemName: "lock.open")
+                                .font(.body.bold())
+                        })
                     })
+                })
                 #endif
         }
     }
